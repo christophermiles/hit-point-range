4f636205d50a692d1a77e88fbcfc7f568d9897fd
christophermiles <christopher.james.miles@googlemail.com>
2024-10-07 12:54:32 +1100

    Basic template

--- main.js
+++ main.js
@@ -3,6 +3,78 @@ class HitPointSpread extends HTMLElement {
         super()
 
         this.attachShadow({ mode: 'open' })
+
+        // region Template
+        const template = document.createElement('template')
+        template.innerHTML = `
+            <style>
+                table {
+                    width: 100%;
+                    border-collapse: collapse;
+                }
+                th, td {
+                    border: 1px solid transparent;
+                    padding: 8px;
+                    text-align: left;
+                }
+            </style>
+            
+            <div>
+                <form id="hitpoint-form">
+                    <slot name="form-content">
+                        <label for="dice-number">Number of dice: </label>
+                        <input type="number" id="dice-number" name="dice-number" placeholder="eg. 4">
+                        
+                        <label for="dice-type">Dice type: </label>
+                        <input type="text" id="dice-type" name="dice-type" placeholder="eg. d8">
+                        
+                        <label for="bonus">Bonus: </label>
+                        <input type="number" id="bonus" name="bonus" placeholder="eg. +4">
+                        
+                        <button type="submit">Calculate</button>
+                    </slot>
+                </form>
+
+                <div id="result">
+                    <slot name="result-table">
+                        <!-- Default table structure -->
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Type</th>
+                                    <th>Value</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Minimum</td>
+                                    <td id="min-hp">&mdash;</td>
+                                </tr>
+                                <tr>
+                                    <td>Weak</td>
+                                    <td id="weak-hp">&mdash;</td>
+                                </tr>
+                                <tr>
+                                    <td>Average</td>
+                                    <td id="avg-hp">&mdash;</td>
+                                </tr>
+                                <tr>
+                                    <td>Strong</td>
+                                    <td id="strong-hp">&mdash;</td>
+                                </tr>
+                                <tr>
+                                    <td>Maximum</td>
+                                    <td id="max-hp">&mdash;</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </slot>
+                </div>
+            </div>
+        `
+
+        this.shadowRoot.appendChild(template.content.cloneNode(true))
+        // endregion
     }
 }
 
